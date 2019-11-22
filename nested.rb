b038c1f452c5bed15f8a408f7f1d42858a12e6ac
Jordan Hayes <nosafoundation@gmail.com>
2019-11-22 02:14:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -123,7 +123,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-		 alan_kay => {
+		 alan_kay = {
 			:known_for => "Object Orientation", "GUI"
 			:languages => ["Smalltalk", "LISP"]
 		 }
